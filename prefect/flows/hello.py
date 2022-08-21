196d5ea4aa3c1300b17622943749ce0e37864c37
Nathan Nowack <thrast36@gmail.com>
2022-08-21 01:14:20 -0500

    update flow

--- prefect/flows/hello.py
+++ prefect/flows/hello.py
@@ -1,4 +1,5 @@
-from prefect import flow, task
+from operator import is_
+from prefect import flow, get_run_logger, task
 from typing import List
 
 
@@ -9,4 +10,8 @@ def is_even(x: int) -> bool:
 
 @flow
 def stoat(numbers: List[int] = [1, 2, 3]):
-    print(numbers)
+    logger = get_run_logger()
+
+    n_evens = sum(int(is_even(n)) for n in numbers)
+
+    logger.info(f"There were {n_evens} in `numbers`")
